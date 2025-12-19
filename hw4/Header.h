134525ff99d0a7e1a0def76a4d1622e0ee4baf08
tom from earth <t110310238@ntut.edu.tw>
2025-12-19 15:07:23 +0800

    Add files via upload

---
+++ hw4/Header.h
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+#include <Windows.h>
+#include <malloc.h>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d.hpp>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <stdio.h>
+#include <math.h>
+#include <cstdlib>
+#include <chrono>
+#include <cfloat> // ¥Î©ó DBL_MAX, DBL_MIN
+#include <random>
+#include <limits>
+using namespace cv;
+using namespace std;
+
+
+//utility functions
+
+
+// HW4 functions
+void a();
+void b();
+void c();
+
+#endif  // MYHEADER_H#pragma once
